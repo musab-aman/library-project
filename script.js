b01aa8271b3c9b0764d6cb00d0c37206af2e859f
musabaman11 <musabaman11@gmail.com>
2025-10-06 18:26:20 +0300

    addbooklibrary function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const { createElement } = require("react");
+
 const myLibrary = [];
 
 
@@ -11,7 +13,9 @@ function Book(title, author, pages, read) {
 }
 
 const book1 = new Book("The titanic", "tony stark", 222, true);
+myLibrary.push(book1);
 const book2 = new Book("Math", "devil", 2000, false);
+myLibrary.push(book2);
 
 
 
@@ -22,7 +26,22 @@ function addBookToLibrary(title, author, pages, read) {
 
 }
 myLibrary.forEach(element => {
-	
+	const div = document.createElement("div");
+	document.body.append(div);
+	div.className = 'book';
+	const p = document.createElement('p');
+	p.textContent = `title: ${element.title}`;
+	div.appendChild(p);
+	const s = document.createElement('p');
+	s.textContent = `author: ${element.author}`;
+	div.appendChild(s);
+	const v = document.createElement('p');
+	v.textContent = `pages: ${element.pages}`;
+	div.appendChild(v);
+	const r = document.createElement('p');
+	r.textContent = `read: ${element.read}`;
+	div.appendChild(r);
+
 });
 
 
