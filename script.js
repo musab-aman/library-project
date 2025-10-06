f537c2f3290ca70373ab5d93134fa98f650f43dd
musabaman11 <musabaman11@gmail.com>
2025-10-07 00:04:13 +0300

    dialogue

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
-const { createElement } = require("react");
+
 
 const myLibrary = [];
 
 
+
 function Book(title, author, pages, read) {
 	this.title = title;
-    this.id = crypto.randomUUID();
+	this.id = Date.now().toString() + Math.random().toString(36).substring(2, 9);
 	this.author = author;
 	this.pages = pages;
 	this.read = read;
-	
+	this.info = function() {
+		return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'read' : 'not read yet'}`;
+	};
 }
+
 
 const book1 = new Book("The titanic", "tony stark", 222, true);
 myLibrary.push(book1);
 const book2 = new Book("Math", "devil", 2000, false);
 myLibrary.push(book2);
+const book3 = new Book("fiker eske mekabir", "meheretu", 400, true);
+myLibrary.push(book3);
+
 
 
 
 function addBookToLibrary(title, author, pages, read) {
-    const book = new book(title, author, pages, read);
+    const book = new Book(title, author, pages, read);
 	myLibrary.push(book);
 
 
@@ -42,6 +49,17 @@ myLibrary.forEach(element => {
 	r.textContent = `read: ${element.read}`;
 	div.appendChild(r);
 
+});
+
+const mydial = document.querySelector('.my-dialog');
+const add = document.querySelector('.add');
+const close = document.querySelector('.close');
+
+add.addEventListener('click', () => {
+	mydial.showModal();
+})
+close.addEventListener('click', () => {
+    mydial.close();
 });
 
 
